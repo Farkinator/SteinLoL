77c9d66b94ac8212608002e7e336fe34df55c56d
Paul <thefarkinator@gmail.com>
2014-11-08 06:15:28 +0000

    pushing to remote now

---
+++ README.txt
@@ -0,0 +1,89 @@
+-----------------------------------------------------
+DAVID STEINER RIOT APPLICATION
+-----------------------------------------------------
+Hi, this is my spell efficiency cruncher, creatively named
+
+SteinLoL.
+
+=================== USAGE ==================
+Install python 2.7.8 and requests on your computer (requires pip to install)
+
+I AM RUNNING 2.7.8, I APOLOGIZE IF THERE ARE ANY ERRORS, BUT THE PYTHON COMMUNITY
+IS VERY DIVIDED ON WHICH VERSION PEOPLE SHOULD USE.
+
+Python:
+https://www.python.org/downloads/
+
+Requests:
+http://docs.python-requests.org/en/latest/user/install/#install
+
+PIP:
+https://pip.readthedocs.org/en/latest/installing.html
+
+then run:
+
+python steinLoL.py
+
+Input cues should take it from there.
+!!!!!!!!IMPORTANT!!!!!!
+I want you to input CDR as a whole number, not a decimal.
+That means, if you want 20% CDR, just input 20, NOT .2
+
+
+================ EFFICIENCY? =================
+
+To me, there are two types of efficiency when
+we talk about spells. I'll call them damage
+efficiency and stat efficiency. 
+
+Damage efficiency: Simply Damage per second per
+cost unit (mana/energy/heat/what-have-you):
+
+d = damage
+m = mana
+c = spell casts.
+	  BaseDamage(d/c) + ADcoefficient(d/AD) * AD + APcoefficient * AP
+DPS=  ----------------------------
+	             cooldown(s/c)
+Dimensional analysis proves this a solid formula:
+
+1 spell    |  300 Damage
+20 seconds |  1 spell
+^Inverse of Cool Down, which is seconds/spell
+DE = DPS/cost, final unit of
+Damage/(mana * seconds)
+
+
+
+Stat efficiency (SE): What spell improves its DPS/mana
+the most with these stats? Here's my equation:
+
+DEi = Damage Efficiency improved by stats
+DEo = Base Damage Efficiency
+
+SE = DEi/DEo
+
+The ratio DEi/DEo will tell us how well the stats affect
+a certain spell. A high Stat Efficiency is a good thing.
+
+
+========================= HEURISTICS AND OTHER CONSIDERATIONS ==============
+DoT's don't normally stack. If it is a DoT, the duration of the DoT is added to the cooldown
+in a 1v1 situation.
+
+AoE DoT (Lookin at you, Tormented Soil/Absolute Zero): an interesting problem. Will get to it on Sunday.
+Two profiles(?):
+	* Optimistic: full duration, enemies stand in it the AoE for full duration
+	* Realistic: Half duration :)
+	* Mean: minimum duration. You missed your chance, bub.
+
+
+
+
+
+=============================== LICENSE ====================================
+
+MIT. I hope no one plagiarizes me, that wouldn't be cool (FINGERS CROSSED KIDS)
+
+
+-DAVID STEINER (THEFARKINATOR)
---
+++ steinLoL.py
@@ -0,0 +1,77 @@
+#The real workhorse here is requests. What a guy. http://docs.python-requests.org/en/latest/
+import requests
+##########################################################################
+#### 					PLEASE READ THE README.TXT					  ####
+####					FOR AN EXPLANATION OF CODE					  ####	
+##########################################################################
+def processSpell(spell, attack, ability, cdr):
+	(efficiency, maxdamage, apscale, adscale, base, dps) = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+	#Basically, ignore toggles for now.
+	maxrank = spell["maxrank"]
+	cd = spell["cooldown"][maxrank-1] * ((100-cdr)/100)
+	if spell["costType"] == ("Mana" or "Health" or "Heat" or "Energy"):
+		
+
+		
+		#If the damage field is null, then our damage is 0.
+		if spell["effect"][1] == None:
+			return {"dps": dps, "maxd": maxdamage, "ap": apscale, "ad": adscale, "base": base, "efficiency": efficiency, "cd":cd}
+		else:
+			#Here we go boys. 
+			base = spell["effect"][1][maxrank-1]
+			if "vars" in spell:
+				for i in spell["vars"]:
+					#vars is our list of scalers. Might throw in mana/souls later, but these are hard to account for b/c they change so often.
+					#for now, I'll stick to AP and AD.
+					if i["link"] == "spelldamage":
+						apscale = ability*i["coeff"][0] #Bonus damage = AP*APc
+					elif i["link"] == "attackdamage":
+						adscale = attack*i["coeff"][0] #Bonus damage = AD * ADc
+			maxdamage = base+apscale+adscale
+			#This is the most basic measure I could come up with. Damage/Cooldown = dps.
+			if cd != 0:
+				dps = maxdamage/cd
+			#So we have our maxdamage! maxdamage != efficient, however. More calculations to come.
+
+
+			return {"dps": dps, "maxd": maxdamage, "ap": apscale, "ad": adscale, "base": base, "efficiency": efficiency, "cd":cd}
+	else:
+		return {"dps": dps, "maxd": maxdamage, "ap": apscale, "ad": adscale, "base": base, "efficiency": efficiency, "cd":cd}
+
+
+
+#Gotta declare these as doubles so stuff doesn't go haywire with the DEEPS calculations.
+ap_in = 0.0
+ad_in = 0.0
+cdr_in = 0.0
+#Man, python input is dumb sometimes. Oh well, I won't make y'all download any more libraries than request.
+ap_in = float(raw_input("Enter your given Ability Power (AP):\n"))
+ad_in = float(raw_input("And now for your Attack Damage (AD):\n"))
+cdr_in = float(raw_input("Finally, enter your Cooldown Reduction (CDR) as a percent, not a decimal:\n"))
+if cdr_in > 0 and cdr_in < 1:
+	cdr_in = float(raw_input("CDR was malformed (You probably entered it as a decimal, not a whole number). Please try again: \n"))
+#And now, let requests work its magic!
+key = "4a956ce8-2409-442a-a003-8f2784580237"
+r = requests.get('https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=spells&api_key='+key)
+champList= r.json()["data"]
+
+#This is for fun purposes only. I like to see where the numbers are coming from.
+final_dict = {"dps":0, "maxd":0, "ap":0, "ad":0, "base":0, "efficiency": 0, "cd":0}
+maxname=""
+templist = {"dps":0, "maxd":0, "ap":0, "ad":0, "base":0, "efficiency": 0, "cd":0}
+#So, obviously doing this linearly is the best option. O(n*4) is the best I can do since
+#I have to look at every single spell in the game 
+for k, v in champList.iteritems():
+	#Iterate through this champion's spells
+	for i in v["spells"]:
+		#process the spell
+		tempdict = processSpell(i, ad_in, ap_in, cdr_in)
+		#if the efficiency is higher, then overwrite our running max.
+		if  tempdict["dps"] > final_dict["dps"]:
+		 	final_dict = tempdict
+		 	#store the name for pretty-factor.
+		 	maxname = i["name"]
+
+
+print maxname + ": %.2f (%.2f + %.2f(AP) + %.2f(AD)) / %.2f(CD)" % (final_dict["dps"], final_dict["base"], final_dict["ap"], final_dict["ad"], final_dict["cd"])
+
