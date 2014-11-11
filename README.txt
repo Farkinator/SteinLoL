bc6a3aa0f7529558be845a3589e8aa1f520c0db8
Paul <thefarkinator@gmail.com>
2014-11-11 05:02:22 +0000

    Fixed a LOT of bugs. Still need to work on Charge spell cooldowns, as that is currently making Excessive Force, Teemo Trap, & Corki Missiles too efficient.

--- README.txt
+++ README.txt
@@ -68,11 +68,18 @@ a certain spell. A high Stat Efficiency is a good thing.
 
 
 ========================= HEURISTICS AND OTHER CONSIDERATIONS ==============
+
+Dealing with Charges?
+	* Where
+
+(NOTE: THIS IS ONLY IF I HAVE TIME TO DO TEXT PARSING.)
 DoT's don't normally stack. If it is a DoT, the duration of the DoT is added to the cooldown
 in a 1v1 situation.
 
 Teamfight scenario or 1v1?:
-	* 
+	* Hitting multiple heroes DEFINITELY increases your efficiency, just a question of 
+	* 3 profiles: optimistic(2x the damage, 2 enemies hit), realistic (1.5x sometimes you hit 2,
+	sometimes you hit 1. evens out.), and mean(1. At least you hit).
 
 AoE DoT (Lookin at you, Tormented Soil/Absolute Zero): an interesting problem. Will get to it on Sunday.
 Three profiles(?):
@@ -80,9 +87,32 @@ Three profiles(?):
 	* Realistic: Half duration :)
 	* Mean: minimum duration (1 tick). You missed big-time, bub.
 
+============================ SUGGESTIONS/BUGS ===================================
+So, I ran into some challenges using the API. Here are some things I would like
+to improve on should I get the chance:
+
+	* Make the documentation and the JSON key/value pairs more usable.
+	Currently there are some rules I found, but they're broken just enough times
+	to be annoying, and there's little to no consistency outside of where to find
+	the damage (PRIORITY: the "effect" array)
+	* There is no "duration" field on some spells. Tormented Soil's duration, 
+	for example, is not accessible programatically.
+	* There is no way to programatically tell whether or not a spell does damage 
+	over time or instantaneous damage.
+	* Frequently there are mystery numbers in the "effects" array. Examples:
+		- 200/300/400 KarthusRequiem {{e2}} - probably the old mana cost
+		- 80/110/140/170/200 ThreshQ{{e3}} - the old, pre-buff damage
+		- 1/1.25/1.5/1.75/2 ThreshQ {{e2}} - Duration being changed soon?
+		- 4/4/4/4 Lunar Rush {{e1}} - I legitimately don't know what this is
+		- 110/115/120/125/130 {{e2}} and 6/6/6/6/6 {{e3}} Excessive Force
+	  Anyways, this could use some cleaning up.
+	* Riftwalk's tooltip is completely bugged. It doesn't indicate that it scales
+	with mana at all. There used to be AP scaling, but that got patched out.
+	This tooltip bug has been here since March.
 
 
-
+I realize it may take some time to patch in some dependencies, but it's totally
+worth it. The more consistent the API is, the cooler stuff people can do with it!
 
 =============================== LICENSE ====================================
 
--- steinLoL.py
+++ steinLoL.py
@@ -5,22 +5,45 @@ import requests
 ####					FOR AN EXPLANATION OF CODE					  ####	
 ##########################################################################
 
-def processSpell(spell, attack, ability, cdr, is1v1):
-	(dam_eff, maxdamage, apscale, adscale, base, dps, manacost) = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-	#Basically, ignore toggles for now.
+def processSpell(spell, attack, ability, cdr, is1v1, realism):
+	(dam_eff, maxdamage, apscale, adscale, base, dps, s_cost, stat_eff) = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+	
 	maxrank = spell["maxrank"]
+
 	cd = spell["cooldown"][maxrank-1] * ((100-cdr)/100)
-	s_cost = spell["cost"][maxrank-1]
-	if spell["costType"] == ("Mana" or "Health" or "Heat" or "Energy"):
+
+
+	#Sadly, there are some hard-to-overcome limitations in the API. Will hardcode some stuff here.
+	# if spell["key"] == "BantamTrap":
+		#There is no duration field in the API. Time to hard-code for DoT's :)
 		
 
+	s_cost = spell["cost"][maxrank-1]
+	#Basically, this ignores the toggle-abilities.
+	if spell["costType"] == ("Mana" or "Health" or "Heat" or "Energy"):
+		#Find the index for the Damage values.
+		try:
+			value_index = spell["leveltip"]["label"].index("Damage")+1
+		except ValueError:
+			try:
+				value_index = spell["leveltip"]["label"].index("Base Damage")+1
+			except ValueError:
+				value_index = 0
+
+		# Check for some malformed damage value indices (That is, the place they are in the ["leveltip"] array 
+		# doesn't match its position in ["effect"].)
+		if spell["name"] in ["Tantrum", "Bullet Time", "Overload"]:
+			value_index -= 1
+		elif spell["key"] in ["AzirE", "EliseHumanW", "FiddlesticksDarkWind", "XerathLocusOfPower2"]:
+			value_index += 2
+		elif spell["key"] in ["NamiW", "SorakaE", "UFSlash", "CaitlynYordleTrap", "SkarnerImpale", "Volley", "DianaOrbs", "HecarimW", "JarvanIVDemacianStandard", "LucianR"]:
+			value_index += 1
 		
 		#If the damage field is null, then our damage is 0.
-		if spell["effect"][1] == None:
-			return {"dps": dps, "maxd": maxdamage, "ap": apscale, "ad": adscale, "base": base, "dam_eff": dam_eff, "cd":cd}
+		if spell["effect"][value_index] == None:
+			return {"stat": stat_eff, "dps": dps, "maxd": maxdamage, "ap": apscale, "ad": adscale, "base": base, "dam_eff": dam_eff, "cd":cd, "cost": s_cost}
 		else:
-			#Here we go boys. 
-			base = spell["effect"][1][maxrank-1]
+			base = spell["effect"][value_index][maxrank-1] # Normal thing.
 			if "vars" in spell:
 				for i in spell["vars"]:
 					#vars is our list of scalers. Might throw in mana/souls later, but these are hard to account for b/c they change so often.
@@ -31,17 +54,31 @@ def processSpell(spell, attack, ability, cdr, is1v1):
 						adscale = attack*i["coeff"][0] #Bonus damage = AD * ADc
 			#So we have our maxdamage! maxdamage != efficient, however. More calculations to come.
 			maxdamage = base+apscale+adscale
+			#Time for some teamfight considerations.
+			if is1v1 == "false":
+				#Karthus will ALWAYS do 5x damage with Requiem in a teamfight. Unless, of course, it gets silenced. That'd be mean.
+				if spell["key"] == "KarthusFallenOne" and realism != "m":
+					maxdamage*=5
+					base*=5
+
+
 			#This is the most basic measure I could come up with. Damage/Cooldown = dps.
 			if cd != 0:
 				dps = maxdamage/cd
-				if s_cost != 0:
-					dam_eff = dps/s_cost
+				b_dps = base/spell["cooldown"][maxrank-1] #For stat efficiency, we need our base dps (dps without ap, cdr, or ad)
+				if s_cost == 0:
+					s_cost = 1 #Account for no-cost spells. IDK if there's any better way to really do that.
+				dam_eff = dps/s_cost
+				b_eff = b_dps/s_cost
+				#Which spell will use its stats most efficiently? (efficiency with stats)/(efficiency without ap/ad/cdr)
+				stat_eff = dam_eff/b_eff
 
 
+			
 
-			return {"dps": dps, "maxd": maxdamage, "ap": apscale, "ad": adscale, "base": base, "dam_eff": dam_eff, "cd":cd}
+			return {"stat": stat_eff, "dps": dps, "maxd": maxdamage, "ap": apscale, "ad": adscale, "base": base, "dam_eff": dam_eff, "cd":cd, "cost": s_cost}
 	else:
-		return {"dps": dps, "maxd": maxdamage, "ap": apscale, "ad": adscale, "base": base, "dam_eff": dam_eff, "cd":cd}
+		return {"stat": stat_eff, "dps": dps, "maxd": maxdamage, "ap": apscale, "ad": adscale, "base": base, "dam_eff": dam_eff, "cd":cd, "cost": s_cost}
 
 
 
@@ -49,35 +86,40 @@ def processSpell(spell, attack, ability, cdr, is1v1):
 ap_in = 0.0
 ad_in = 0.0
 cdr_in = 0.0
+
 #Man, python input is dumb sometimes. Oh well, I won't make y'all download any more libraries than request.
 ap_in = float(raw_input("Enter your given Ability Power (AP):\n"))
 ad_in = float(raw_input("And now for your Attack Damage (AD):\n"))
 cdr_in = float(raw_input("Finally, enter your Cooldown Reduction (CDR) as a percent, not a decimal:\n"))
 if cdr_in > 0 and cdr_in < 1:
 	cdr_in = float(raw_input("CDR was malformed (You probably entered it as a decimal, not a whole number). Please try again: \n"))
-is1v1 = bool(raw_input("1v1(true) or Teamfight?(false)"))
+is1v1 = raw_input("1v1(true) or Teamfight?(false)\n")
+realism = raw_input("Realism with regards to aoe/skillshots: mean(m), realistic(r), optimistic(o)\n")
+
 #And now, let requests work its magic!
 key = "4a956ce8-2409-442a-a003-8f2784580237"
 r = requests.get('https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=spells&api_key='+key)
 champList= r.json()["data"]
 
 #This is for fun purposes only. I like to see where the numbers are coming from.
-final_dict = {"dps":0, "maxd":0, "ap":0, "ad":0, "base":0, "dam_eff": 0, "cd":0}
+final_dict = {"stat": 0, "dps":0, "maxd":0, "ap":0, "ad":0, "base":0, "dam_eff": 0, "cd":0, "cost": 0}
 maxname=""
-templist = {"dps":0, "maxd":0, "ap":0, "ad":0, "base":0, "dam_eff": 0, "cd":0}
+templist = {"stat": 0, "dps":0, "maxd":0, "ap":0, "ad":0, "base":0, "dam_eff": 0, "cd":0, "cost": 0}
 #So, obviously doing this linearly is the best option. O(n*4) is the best I can do since
 #I have to look at every single spell in the game 
 for k, v in champList.iteritems():
 	#Iterate through this champion's spells
 	for i in v["spells"]:
 		#process the spell
-		tempdict = processSpell(i, ad_in, ap_in, cdr_in, is1v1)
+		tempdict = processSpell(i, ad_in, ap_in, cdr_in, is1v1, realism)
 		#if the dam_eff is higher, then overwrite our running max.
-		if  tempdict["dam_eff"] > final_dict["dam_eff"]:
+		if  tempdict["stat"] > final_dict["stat"]:
 		 	final_dict = tempdict
 		 	#store the name for pretty-factor.
 		 	maxname = i["name"]
+		 	champ = v["name"]
 
-
-print maxname + ": %.2f (%.2f + %.2f(AP) + %.2f(AD)) / %.2f(CD)" % (final_dict["dam_eff"], final_dict["base"], final_dict["ap"], final_dict["ad"], final_dict["cd"])
-
+print maxname + " ("+champ+") is the most efficient spell!"
+print "\tStat Efficiency: %.2f" %(final_dict["stat"]) 
+print "\tDamage Efficiency: %.2f (%.2f(DPS)/%.2f(Spell Cost))" %(final_dict["dam_eff"], final_dict["dps"], final_dict["cost"])
+print "\tDamage: %.2f (%.2f + %.2f(AP) + %.2f(AD)) / %.2f(CD)" % (final_dict["maxd"], final_dict["base"], final_dict["ap"], final_dict["ad"], final_dict["cd"])
